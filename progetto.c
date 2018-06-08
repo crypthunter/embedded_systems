b01f086cd2c76869391f867d8ced7a7eeb39f777
Lorenzo <l.dalessandro5@campus.unimib.it>
2018-06-08 13:57:37 +0200

    added accelerometer interrupt

--- progetto.c
+++ progetto.c
@@ -1,18 +1,63 @@
 #include <c8051f020.h> // SFR definitions
-//bottone e led
+
+//---------------------------------------------------VARIABILI----------------------------------------------------------------------
+// 0 accelerometro, 1 temperatura, 2 display
+unsigned char select_interrupt;
+//-----------------------------------------------------------PWM--------------------------------------------------------------------
+//bottone e backlight
 sbit button = P3^7;
-sbit led = P0^6;
+sbit backlight = P0^6;
 //contatore per il timer1
 unsigned char t1overFlow = 0;
 // 0 = bottone non premuto, 1 = bottone premuto
 unsigned char premuto = 0;
 //se è passato più di un secondo da quando il bottone è stato premuto
 unsigned char unSec = 0;
-//variabile per decidere se il led deve lampeggiare ( molto velocemente così da sembrare ad una luminosità differente)
+//variabile per decidere se il backlight deve lampeggiare ( molto velocemente così da sembrare ad una luminosità differente)
 //oppure restare spento
 unsigned char acceso = 1;
 //variabile per controllare la luminosità
 unsigned char lumi;
+//--------------------------------------------COSTANTI PERIFERICHE-----------------------------------------------------------------
+#define SMB_START 0x08 //ricevuto start
+#define SMB_RESTART 0x010 //ricevuto restart
+
+#define SMB_FIRSTWRITE 0x18 //scrivo il primo valore
+#define SMB_WRITE 0x28 // scrivo gli altri valori
+
+#define SMB_FIRSTREAD 0x40  //primo read
+#define SMB_READ 0x50 //altri read
+#define SMB_READ_NACK 0x58  //dopo che ho dato AA = 0
+
+#define MMA_WRITE 0x98 //indirizzo per scrivere sull'accelerometro
+#define MMA_READ 0x99  //indirizzo per leggere dall'accelerometro
+
+#define TMP_READ 0x91  //indirizzo per leggere dal termometro
+
+#define DISPLAY_WRITE 0x7c  //indirizzo per leggere dal display
+
+//--------------------------------------------------------ACCELEROMETRO-------------------------------------------------------------
+//definizione registri accelerometro
+#define 	XOUT				0x00
+#define 	YOUT				0x01
+#define 	ZOUT				0x02
+#define 	MODE				0x07
+
+unsigned char mma_init [] = {MODE, 0x01};
+unsigned char mma_pos = 0;
+unsigned char mma_init_finished = 0;
+unsigned char mma_read_ready = 0;
+int mma_value_read = 0;
+int i = 0;
+float xyz[3];
+unsigned char xyz_mma_pos = 0;
+code float TILT_XY[64] = {0, 2.69, 5.38, 8.08, 10.81, 13.55, 16.33, 19.16, 22.02, 24.95, 27.95, 31.04, 34.23, 37.54, 41.01, 44.68, 48.59, 52.83, 57.54, 62.95, 69.64, 79.86, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+  0, 0, 0, 0, 0, 0, 0, -79.86, -69.64, -62.95, -57.54, -52.83, -48.59, -44.68, -41.01, -37.54, -34.23, -31.04, -27.95, -24.95, -22.02, -19.16, -16.33, -13.55, -10.81, -8.08, -5.38, -2.69}; 
+code float TILT_Z[64] = {90.00, 87.31, 84.62, 81.92, 79.19, 76.45, 73.67, 70.84, 67.98, 65.05, 62.05, 58.96, 55.77, 52.46, 48.99, 45.32, 41.41, 37.17, 32.46, 27.05, 20.36, 10.14, 0, 0, 0, 0, 0, 0, 
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -10.14, -20.36, -27.05, -32.46, -37.17, -41.41, -45.32, -48.99, -52.46, -55.77, -58.96, -62.05, -65.05, -67.98, -70.84, -73.67, -76.45, -79.19, -81.92, -84.62};
+
+	
+//---------------------------------------PROGRAMMA-------------------------------------------
 void init (void) {
 	//abilita iinterrupt globali
 	EA = 1;
@@ -22,10 +67,12 @@ void init (void) {
 	//oscillatore
 	OSCICN &= 0x14;
 	//clock interno
-	XBR0=0x00;
+	XBR0 = 0x00;
+	//abilita smbus e uart ( per spostare i pin)
+	XBR0 |= 0x05;
 	//crossbar, gestisce i pin
 	XBR1 = 0x00;
-	XBR2=0x40;
+	XBR2 = 0x40;
 	//push-pull
 	P1MDOUT |= 0x40;
 	//push pull display
@@ -36,7 +83,14 @@ void init (void) {
 	EIE1 |= 0x02;
 	//abilita smbus
 	ENSMB = 1;
+	AA = 1;
 }
+
+/*
+																	**********************************************************
+																	***********************GESTIONE PWM***********************
+																	**********************************************************
+*/
 
 //resetta il timer 2 senza fermarlo
 void resetTimer2()
@@ -52,7 +106,7 @@ void setLumi()
 	TR2 = 1;
 }
 
-//quando il timer va in iinterrupt sono passati 200 ms, incrementa di poco la luminosità del led
+//quando il timer va in iinterrupt sono passati 200 ms, incrementa di poco la luminosità del backlight
 void timer2() interrupt 5
 {
 	char lumiStep = 1;
@@ -70,20 +124,20 @@ void resetTimer1()
 		TR1 = 0;
 }
 
-//imposta il necessario per il programma
+//immma_posta il necessario per il programma
 void pwm_setup()
 {
 	//timer 0 a 8 bbit, timer 1 a 16 bbit
 	TMOD = 0x10;
 	//clock intero per il timer0
 	CKCON = 0x8;
-	//luminosità iniziale del led, 0 = luminosità massima
+	//luminosità iniziale del backlight, 0 = luminosità massima
 	lumi = 100;
 	//abilita gli iinterrupt di timer0, timer1 e timer2
 	ET0 = 1;
 	ET1 = 1;
 	ET2 = 1;
-	//imposta il valore iniziale del timer 1 (conta 200 ms)
+	//immma_posta il valore iniziale del timer 1 (conta 200 ms)
 	TH1 = 0x7d;
 	TL1 = 0xcb;
 	//fa partire il timer 0 che gestisce pwm
@@ -93,16 +147,16 @@ void pwm_setup()
 //iinterrupt del timer0
 void timer0() interrupt 1
 {
-	//il led lampeggia solo se la variabile è a 1
+	//il backlight lampeggia solo se la variabile è a 1
 	if (acceso == 1)
 	{
-		if (!led) {	
-			led = 1;		// cambia stato led
+		if (!backlight) {	
+			backlight = 1;		// cambia stato backlight
 			TH0 = lumi;	
 			TF0 = 0;		//pulisce flag interrput
 		}
 		else {			
-			led = 0;
+			backlight = 0;
 			TH0 = 255 - lumi;	
 			TF0 = 0;
 		}
@@ -123,7 +177,7 @@ void buttonInt() interrupt 19
 		P3IF |= 0x08;
 		premuto = 1;
 	}
-	//se sto premendo il bottone e quando rilascio non è passato un secondo, deve cambiare lo stato del led
+	//se sto premendo il bottone e quando rilascio non è passato un secondo, deve cambiare lo stato del backlight
 	else if (premuto == 1 && unSec == 0)
 	{
 		//resetto l'interrupt
@@ -131,9 +185,9 @@ void buttonInt() interrupt 19
 		P3IF &= 0x7f;
 		//rimette in falling edge il bottone
 		P3IF &= 0x77;
-		//cambio stato led
+		//cambio stato backlight
 		acceso = !acceso;
-		led = 0;
+		backlight = 0;
 		premuto = 0;
 		unSec = 0;
 		//resetto e disabilito il timer
@@ -166,7 +220,7 @@ void timer1() interrupt 3
 		//fermo il timer
 		resetTimer1();
 		unSec = 1;
-		//imposto la luminosità solo se il bottone è acceso
+		//immma_posto la luminosità solo se il bottone è acceso
 		if (acceso == 1)
 		{
 			setLumi();
@@ -180,9 +234,93 @@ void timer1() interrupt 3
 	}
 }
 
+/*
+																	**********************************************************
+																	***********************ACCELEROMETRO**********************
+																	**********************************************************
+*/
+
+void accelerometer_interrupt()
+{
+	
+	if(mma_pos == sizeof(mma_init))
+			{
+				mma_pos = 0;
+				mma_init_finished = 1;
+				//STO = 1;
+				//STA = 1;
+			}
+			switch(SMB0STA)
+			{
+				case SMB_START:
+					SMB0DAT = MMA_WRITE;
+					STA = 0;
+					break;
+				
+				//gli devo dare indirizzo di lettura
+				case SMB_RESTART:
+					SMB0DAT = MMA_READ;
+					STA = 0;
+					break;
+
+				case SMB_FIRSTWRITE:
+				case SMB_WRITE:
+					if(mma_init_finished == 0)
+					{
+						SMB0DAT = mma_init[mma_pos];
+						mma_pos++;
+					}
+					else if(mma_init_finished == 2)
+					{
+						if(!mma_read_ready)
+						{
+							SMB0DAT = XOUT;
+							mma_read_ready = 1;
+						}
+						else
+							STA = 1;
+					}
+					break;
+					
+				case SMB_FIRSTREAD:
+					STA = 0;
+					break;
+				
+				case SMB_READ:
+					mma_value_read = SMB0DAT;
+					mma_value_read &= 00111111;
+					xyz[xyz_mma_pos] = TILT_XY[mma_value_read];
+					if(xyz_mma_pos == 1)
+						AA = 0;
+				
+					xyz_mma_pos++;
+					break;
+				
+				case SMB_READ_NACK:
+					xyz[xyz_mma_pos] = TILT_Z[mma_value_read];
+					STO = 1;
+					AA = 1;
+					
+			}
+			SI = 0;
+}
+
+void smBus() interrupt 7
+{
+	//i = 100;
+	accelerometer_interrupt();
+}
+
 void main()
 {
 	init();
 	pwm_setup();
+	STA = 1;
+	while(!mma_init_finished);
+	STO = 1;
+	for(i = 0; i < 1000; i++);
+	mma_init_finished = 2; 
+	STA = 1;
+	
 	while(1);
 }
